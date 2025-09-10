8cb913c475f5c6428f92d79a91860a08a826dbd9
saeed.taraan <saeed.taraan@gmail.com>
2025-09-10 12:05:53 +0330

    Initial Entities Created

---
+++ Contract/SabaRayane.Contract.Domain/Aggregates/CustomerAggregate/Contract.cs
@@ -0,0 +1,32 @@
+﻿using SabaRayane.Contract.Domain.Aggregates.ProductAggregate;
+using Taran.Shared.Core.Entity;
+using Taran.Shared.Core.Exceptions;
+
+namespace SabaRayane.Contract.Domain.Aggregates.CustomerAggregate;
+
+public class Contract : BaseEntity<int>
+{
+    public int CustomerId { get; private set; }
+    public Customer Customer { get; private set; }
+
+    public int ProductId { get; private set; }
+    public Product Product { get; private set; }
+
+    public long Price { get; private set; }
+
+    public DateOnly ContractDate { get; private set; }
+
+    public int DurationInMonths { get; private set; }
+
+    public int ExtraUsersCount { get; private set; }
+
+    internal Contract(int creatorUserId, int productId, long price, DateOnly contractDate, int durationInMonths, int extraUsersCount)
+        : base(creatorUserId)
+    {
+        ProductId = productId;
+        Price = price > 0 ? price : throw new DomainInvalidArgumentException(nameof(Price));
+        ContractDate = contractDate;
+        DurationInMonths = durationInMonths > 0 ? durationInMonths : throw new DomainInvalidArgumentException(nameof(DurationInMonths));
+        ExtraUsersCount = extraUsersCount >= 0 ? extraUsersCount : throw new DomainInvalidArgumentException(nameof(ExtraUsersCount));
+    }
+}
---
+++ Contract/SabaRayane.Contract.Domain/Aggregates/ProductAggregate/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Taran.Shared.Core.Entity;
+using Taran.Shared.Core.Exceptions;
+
+namespace SabaRayane.Contract.Domain.Aggregates.ProductAggregate;
+
+public class Product : AggregateRoot<int>
+{
+    [StringLength(50)]
+    public string Name { get; private set; }
+
+    public long Price { get; private set; }
+
+    [StringLength(500)]
+    public string Description { get; private set; }
+
+    public Product(int creatorUserId, string name, long price, string description) : base(creatorUserId)
+    {
+        Name = DomainArgumentNullOrEmptyException.Ensure(name, nameof(Name));
+        Price = price > 0 ? price : throw new DomainInvalidArgumentException(nameof(Price));
+        Description = description;
+    }
+}
